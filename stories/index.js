b195e63e8726ace66a9f41adba798175661d6b6d
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-01-20 13:59:39 -0800

    Change interviewer to value

--- stories/index.js
+++ stories/index.js
@@ -130,7 +130,7 @@ storiesOf("InterviewerList", module)
   .add("Preselected", () => (
     <InterviewerList
       interviewers={interviewers}
-      interviewer={3}
+      value={4}
       setInterviewer={action("setInterviewer")}
     />
   ));
