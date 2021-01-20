9e654363066bcddc4ab78b35917de4fb3e7e9e0f
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-01-19 22:59:52 -0800

    Change component Form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,19 @@
 import React from "react";
+
 import "components/Appointment/styles.scss";
 
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
+  return (
+    <>
+      <article className="appointment"></article>
+      <Header time={props.time} />
+    </>
+  );
 }
