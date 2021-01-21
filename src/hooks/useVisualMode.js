719ba2312c9ee0b03aace57d1f8094c2f973408b
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-01-21 15:18:01 -0800

    Add back function to the custom hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,27 +1,25 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [history, setHistory] = useState([]);
   const [mode, setMode] = useState(initial);
-
-  function transition(newMode, replace) {
-    setMode(newMode);
-  }
-  return { mode, transition };
+  const [history, setHistory] = useState([initial]);
+  const transition = function (newMode, replace = false) {
+    if (replace) {
+      setMode(newMode);
+      setHistory([...history.slice(0, -1), newMode]);
+    } else {
+      setMode(newMode);
+      const newHistory = [...history, newMode];
+      setHistory(newHistory);
+    }
+  };
+  const back = function () {
+    if (history.length < 2) {
+      return;
+    } else {
+      setHistory(history.slice(0, -1));
+      setMode(history[history.length - 2]);
+    }
+  };
+  return { mode, transition, back };
 }
-
-// import { useState, useEffect } from "react";
-// export default function useVisualMode() {
-//   const [history, setHistory] = useState([initial]);
-//   const transition = function (mode, replace = false) {
-//     setHistory([...prev, mode])
-//   }
-//   const back = function () {
-//     if (history.length < 2) {
-//       return;
-//     }
-//     setHistory(prev => [...prev.slice(0, prev.length - 1)]);
-//     const mode = history.slice(-1)[0];
-//     return {mode, transition, back}
-//   }
-// }
