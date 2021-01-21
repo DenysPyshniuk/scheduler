480fd98046bbaa7f1cf4b80d1352db04bdbfc8c7
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-01-21 11:18:46 -0800

    Get interviews from the state

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,30 +1,51 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "components/Application.scss";
 
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
-import getAppointmentsForDay from "helpers/selectors"
-import "components/Application.scss";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {},
+    interviewers: {},
   });
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-  const setDay = day => setState({ ...state, day });
+  const setDay = (day) => setState({ ...state, day });
+  const appointments = getAppointmentsForDay(state, state.day);
+
+  // interviewers data
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   //renders data for days (nav bar)
   useEffect(() => {
-Promise.all([
-  axios.get("/api/days"),
-  axios.get("/api/appointments"),
-  axios.get("/api/interviewers")
-]).then((all) => {
-  const [days, appointments, interviewers] = all;
-  setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-});
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then((all) => {
+      const [days, appointments, interviewers] = all;
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+    });
   }, []);
 
   return (
@@ -46,6 +67,7 @@ Promise.all([
         />
       </section>
       <section className="schedule">
+        {schedule}
         {dailyAppointments.map((appointment) => {
           return <Appointment key={appointment.id} {...appointment} />;
         })}
