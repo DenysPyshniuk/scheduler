b6fe16a558b5b9bfdb23aec6e5af21fdae48d7b3
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-01-20 19:28:04 -0800

    Implement function to get appointment for the day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,11 @@
+export function getAppointmentsForDay(state, day) {
+  let appointments = [];
+  state.days.forEach((eachDay) => {
+    if (eachDay.name === day) {
+      for (const id of eachDay.appointments) {
+        appointments.push(state.appointments[id]);
+      }
+    }
+  });
+  return appointments;
+}
