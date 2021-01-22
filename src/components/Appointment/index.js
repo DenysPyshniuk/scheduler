96044f7461c4f4ad9f02118c2773019ed8c6ea62
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-01-21 16:57:19 -0800

    Fix props undefined problem

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,24 +1,34 @@
 import React from "react";
 
 import "components/Appointment/styles.scss";
-
+import useVisualMode from "hooks/useVisualMode";
+//import components
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <>
+      <article className="appointment"></article>
       <Header time={props.time} />
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer.name}
         />
-      ) : (
-        <Empty />
       )}
-      <article className="appointment"></article>
+
+      {mode === CREATE && <Form interviewers={[]} onCancel={back} />}
     </>
   );
 }
