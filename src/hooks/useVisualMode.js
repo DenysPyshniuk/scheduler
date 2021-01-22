0ae3c3c33d0ed8bbe6415f216d6c179332f37e19
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-01-21 16:57:48 -0800

    Add one more sample of same code

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -23,3 +23,31 @@ export default function useVisualMode(initial) {
   };
   return { mode, transition, back };
 }
+
+// import { useState } from "react";
+
+// // HOOK FOR TRANSITIONING BETWEEN MODES:
+// const useVisualMode = (initialMode) => {
+//   const [history, setHistory] = useState([initialMode]);
+
+//   const transition = function (mode, replace = false) {
+//     if (replace) {
+//       setHistory((prev) => [...prev.slice(0, -1), mode]);
+//     } else {
+//       setHistory((prev) => [...prev, mode]);
+//     }
+//   };
+
+//   const back = () => {
+//     setHistory((prev) => {
+//       if (prev.length > 1) {
+//         return prev.slice(0, -1);
+//       }
+//       return prev;
+//     });
+//   };
+
+//   return { mode: history[history.length - 1], back, transition };
+// };
+
+// export default useVisualMode;
