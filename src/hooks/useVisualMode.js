8cf6e0d1bdbe5be784de9840f0ce2dacdea09e52
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-01-22 17:56:37 -0800

    Prevent stale state in  useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,53 +1,53 @@
 import { useState } from "react";
 
-export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
-  const [history, setHistory] = useState([initial]);
-  const transition = function (newMode, replace = false) {
-    if (replace) {
-      setMode(newMode);
-      setHistory([...history.slice(0, -1), newMode]);
-    } else {
-      setMode(newMode);
-      const newHistory = [...history, newMode];
-      setHistory(newHistory);
-    }
-  };
-  const back = function () {
-    if (history.length < 2) {
-      return;
-    } else {
-      setHistory(history.slice(0, -1));
-      setMode(history[history.length - 2]);
-    }
-  };
-  return { mode, transition, back };
-}
+// export default function useVisualMode(initial) {
+//   const [mode, setMode] = useState(initial);
+//   const [history, setHistory] = useState([initial]);
+//   const transition = function (newMode, replace = false) {
+//     if (replace) {
+//       setMode(newMode);
+//       setHistory([...history.slice(0, -1), newMode]);
+//     } else {
+//       setMode(newMode);
+//       const newHistory = [...history, newMode];
+//       setHistory(newHistory);
+//     }
+//   };
+//   const back = function () {
+//     if (history.length < 2) {
+//       return;
+//     } else {
+//       setHistory(history.slice(0, -1));
+//       setMode(history[history.length - 2]);
+//     }
+//   };
+//   return { mode, transition, back };
+// }
 
 // import { useState } from "react";
 
 // // HOOK FOR TRANSITIONING BETWEEN MODES:
-// const useVisualMode = (initialMode) => {
-//   const [history, setHistory] = useState([initialMode]);
+export default function useVisualMode(initialMode) {
+  const [history, setHistory] = useState([initialMode]);
 
-//   const transition = function (mode, replace = false) {
-//     if (replace) {
-//       setHistory((prev) => [...prev.slice(0, -1), mode]);
-//     } else {
-//       setHistory((prev) => [...prev, mode]);
-//     }
-//   };
+  const transition = function (mode, replace = false) {
+    if (replace) {
+      setHistory((prev) => [...prev.slice(0, -1), mode]);
+    } else {
+      setHistory((prev) => [...prev, mode]);
+    }
+  };
 
-//   const back = () => {
-//     setHistory((prev) => {
-//       if (prev.length > 1) {
-//         return prev.slice(0, -1);
-//       }
-//       return prev;
-//     });
-//   };
+  const back = () => {
+    setHistory((prev) => {
+      if (prev.length > 1) {
+        return prev.slice(0, -1);
+      }
+      return prev;
+    });
+  };
 
-//   return { mode: history[history.length - 1], back, transition };
-// };
+  return { mode: history[history.length - 1], back, transition };
+}
 
 // export default useVisualMode;
