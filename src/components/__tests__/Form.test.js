27974d2a4bc6cddf2cad6a7e44b0d6f576feacd3
Devronder <denys.pyshniuk@gmail.com>
2026-03-08 22:45:02 -0700

    Fix test to verify that interviewer set

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -45,9 +45,8 @@ describe("Form", () => {
 
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText, queryByText } = render(
-      <Form interviewers={interviewers} onSave={onSave} />
-    );
+    const { getByText, getByPlaceholderText, getByAltText, queryByText } =
+      render(<Form interviewers={interviewers} onSave={onSave} />);
 
     fireEvent.click(getByText("Save"));
 
@@ -58,12 +57,15 @@ describe("Form", () => {
       target: { value: "Lydia Miller-Jones" },
     });
 
+    fireEvent.click(getByAltText("Sylvia Palmer"));
+
     fireEvent.click(getByText("Save"));
 
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
+    expect(queryByText(/please select an interviewer/i)).toBeNull();
 
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
 
   it("calls onCancel and resets the input field", () => {
